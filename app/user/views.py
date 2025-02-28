10f1fdcc49bb2753f6f45d56dbdc22ae78ab5563
lastlilith <lastlilith.2014@gmail.com>
2025-02-28 14:35:17 +1000

    changed pre-commit-config

--- app/user/views.py
+++ app/user/views.py
@@ -1,11 +1,8 @@
 """
 Views for the user API
 """
-from rest_framework import (
-    generics,
-    authentication,
-    permissions
-)
+
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -17,17 +14,20 @@ from user.serializers import (
 
 class CreateUserView(generics.CreateAPIView):
     """Create user in a system"""
+
     serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
+
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
+
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
